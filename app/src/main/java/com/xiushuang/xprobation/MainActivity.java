c397eac851bd540148b7745990b5464adf4160ce
zhx <chengxuyuan001@163.com>
2017-02-21 13:39:00 +0800

    初始化support包

--- app/src/main/java/com/xiushuang/xprobation/MainActivity.java
+++ app/src/main/java/com/xiushuang/xprobation/MainActivity.java
@@ -1,13 +1,31 @@
 package com.xiushuang.xprobation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView menuRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findView();
+    }
+
+    private void findView() {
+
+    }
+
+
+    private void menuClick(View view) {
+        int viewId = view.getId();
+        switch (viewId) {
+            default:
+                break;
+        }
     }
 }
